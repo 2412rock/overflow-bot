04d68379064caf8e4dc0bbb4ccf11ca163e6f57f
Adrian Albu <2412rock@gmail.com>
2024-05-26 20:48:35 +0300

    Send oppoent after getting board

--- bot.py
+++ bot.py
@@ -4,14 +4,16 @@ import websockets
 import asyncio
 import random
 import json
+import sys
 
-usernames = ["random_elf", "random_knight", "document_dragon", "spreadsheet_sorcerer", "chart_crusader", "table_troll", "paragraph_paladin", "formatting_fairy", "hyperlink_hero"] 
-username = usernames[random.randint(0, len(usernames)-1)]
+#usernames = ["random_elf", "random_knight", "document_dragon", "spreadsheet_sorcerer", "chart_crusader", "table_troll", "paragraph_paladin", "formatting_fairy", "hyperlink_hero"] 
+username = sys.argv[1]#usernames[random.randint(0, len(usernames)-1)]
+print(f'Running bot with name {username}')
 # URL to make the GET request to
-base_url = "https://overflowapp.xyz:4200" #"http://192.168.1.125:4500"
+base_url = "https://10.132.0.4:4200"#"https://overflowapp.xyz:4200" #"http://192.168.1.125:4500"#
 add_to_queue_url = f"{base_url}/api/addtoqueue"
 get_my_match_url = f"{base_url}/api/getMyMatch?username={username}"
-socket_uri = "wss://overflowapp.xyz:4200/ws/" #"ws://192.168.1.125:4500/ws/" 
+socket_uri = "wss://10.132.0.4:4200/ws/"#"wss://overflowapp.xyz:4200/ws/" #"ws://192.168.1.125:4500/ws/"#
 player_1 = ""
 
 async def connect_and_communicate():
@@ -34,9 +36,10 @@ async def connect_and_communicate():
                 # Send message
                 if "0.1" in message:
                      print("got board")
+                     await websocket.send("opponent")
                 elif "opponent" in message:
                      print("Opponent connected")
-                     await websocket.send("opponent")
+                     #await websocket.send("opponent")
                      time.sleep(2)
                      if player_1 == username:
                          x = random.randint(0, 24)
@@ -59,6 +62,7 @@ async def connect_and_communicate():
                         response = f"{x}:{y}"
                         
                         print("picked: ", response)
+                        time.sleep(random.randint(1,8))
                         await websocket.send(response)
 
                     except json.JSONDecodeError:
@@ -92,10 +96,10 @@ if response.status_code == 200:
               
               while True:
                     response = requests.get(get_my_match_url)
-                    print(response.text)
+                    #print(response.text)
                     response_json = response.json()
                     data =  response_json["data"]
-                    print("Got data from match" , data)
+                    #print("Got data from match" , data)
                     if data["player1"] is not None and data["player2"] is not None:
                         print("Found match")
                         game_id = f'{data["player1"]}-{data["player2"]}'
