ff2b78444a202df0600d6f1ee6bd921b8564f5e2
Adrian Albu <2412rock@gmail.com>
2024-05-26 21:20:05 +0300

    add data check

--- bot.py
+++ bot.py
@@ -100,7 +100,7 @@ if response.status_code == 200:
                     response_json = response.json()
                     data =  response_json["data"]
                     print("Got data from match" , data)
-                    if data["player1"] is not None and data["player2"] is not None:
+                    if data is not None and data["player1"] is not None and data["player2"] is not None:
                         print("Found match")
                         game_id = f'{data["player1"]}-{data["player2"]}'
                         player_1 = data["player1"]
