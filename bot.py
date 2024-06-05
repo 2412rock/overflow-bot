c498551b7b95c85d29f56c9554eb86c6cdaf60cc
Adrian Albu <2412rock@gmail.com>
2024-06-05 16:52:51 +0300

    Add bearer token for matchmaking endpoints

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ import asyncio
 import random
 import json
 import sys
+import os
 
 #usernames = ["random_elf", "random_knight", "document_dragon", "spreadsheet_sorcerer", "chart_crusader", "table_troll", "paragraph_paladin", "formatting_fairy", "hyperlink_hero"] 
 username = sys.argv[1]#usernames[random.randint(0, len(usernames)-1)]
@@ -12,9 +13,11 @@ print(f'Running bot with name {username}')
 # URL to make the GET request to
 base_url = "https://overflowapp.xyz:4200" #"http://localhost:4500"#
 add_to_queue_url = f"{base_url}/api/addtoqueue"
+signin_url = f"{base_url}/api/signin"
 get_my_match_url = f"{base_url}/api/getMyMatch?username={username}"
 socket_uri = "wss://overflowapp.xyz:4200/ws/" #"ws://localhost:4500/ws/"#
 player_1 = ""
+password = os.environ['PASSWORD']
 
 async def connect_and_communicate():
      # Replace with the WebSocket URL
@@ -94,34 +97,56 @@ async def connect_and_communicate():
                 print("WebSocket connection closed")
                 break
 
+def signin():
+    login_req = {"username": username, "password": password}
+    response = requests.post(signin_url, json=login_req)
+    singin_response_json = response.json()
+    if response.status_code == 200 and singin_response_json["isSuccess"] == True:
+        data = singin_response_json["data"]
+        bearer = data[0]
+        print("Got bearer token ", bearer)
+        data = {"username": username}
+        print("Posting data ", add_to_queue_url)
+        headers = {
+        'Authorization': f'Bearer {bearer}',
+        }
+        return headers
+    return None
 
 # Sending the GET request
-data = {"username": username}
-print("Posting data ", add_to_queue_url)
-response = requests.post(add_to_queue_url, json=data)
-if response.status_code == 200:
-        # Printing the response content
-        print("Response content:")
-        print(response.text)
-        response_json =  response.json()
-        if response_json["isSuccess"] == True:
-              
-              while True:
-                    response = requests.get(get_my_match_url)
-                    #print(response.text)
-                    response_json = response.json()
-                    data =  response_json["data"]
-                    #print("Got data from match" , data)
-                    if data is not None and data["player1"] is not None and data["player2"] is not None:
-                        print("Found match")
-                        game_id = f'{data["player1"]}-{data["player2"]}'
-                        player_1 = data["player1"]
-                        socket_uri += f"{game_id}/{username}"
-                        asyncio.run(connect_and_communicate())
-                        break
-                    time.sleep(5)
+headers = signin()
+if headers != None:
+    response = requests.post(add_to_queue_url, headers=headers)
+    if response.status_code == 200:
+            # Printing the response content
+            print("Response content:")
+            print(response.text)
+            response_json =  response.json()
+            if response_json["isSuccess"] == True:
+                
+                while True:
+                        response = requests.get(get_my_match_url, headers=headers)
+                        #print(response.text)
+                        if response.status_code == 200:
+                            response_json = response.json()
+                            data =  response_json["data"]
+                            #print("Got data from match" , data)
+                            if data is not None and data["player1"] is not None and data["player2"] is not None:
+                                print("Found match")
+                                game_id = f'{data["player1"]}-{data["player2"]}'
+                                player_1 = data["player1"]
+                                socket_uri += f"{game_id}/{username}"
+                                asyncio.run(connect_and_communicate())
+                                break
+                        else:
+                             headers = signin()
+                             if headers is None:
+                                  print("Bearer expired and cant sign in again")
+                                  exit(0)
+                        time.sleep(5)
+    else:
+        # If the request failed, print an error message
+        print(f"Error: {response.status_code}")
+        # Checking if the request was successful (status code 200)
 else:
-    # If the request failed, print an error message
-    print(f"Error: {response.status_code}")
-    # Checking if the request was successful (status code 200)
-    
+     print("Failed to login") 
