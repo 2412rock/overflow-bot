1f8dd377b80ef60ba99cc82f93ed46274dd97f27
Adrian Albu <2412rock@gmail.com>
2025-02-26 20:15:49 +0200

    https

--- bot.py
+++ bot.py
@@ -16,11 +16,11 @@ print(f'Running bot with name {username}')
 
 local_ip = os.environ['LOCAL_IP']
 
-base_url = f"http://{local_ip}:4200"#"https://overflowapp.xyz:4200" #"http://localhost:4500"#
+base_url = f"https://{local_ip}:4200"#"https://overflowapp.xyz:4200" #"http://localhost:4500"#
 add_to_queue_url = f"{base_url}/api/addtoqueue"
 signin_url = f"{base_url}/api/signin"
 get_my_match_url = f"{base_url}/api/getMyMatch?username={username}"
-socket_uri = f"ws://{local_ip}:4200/ws/"#"wss://overflowapp.xyz:4200/ws/" #"ws://localhost:4500/ws/"#
+socket_uri = f"wss://{local_ip}:4200/ws/"#"wss://overflowapp.xyz:4200/ws/" #"ws://localhost:4500/ws/"#
 player_1 = ""
 password = os.environ['PASSWORD']
 
